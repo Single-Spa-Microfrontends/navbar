7afcf700e1cd62fb1ea7bd473a30cfa4177ec68f
Oleg Kuibar <oleg.kuibar@a4media.com>
2023-08-28 18:44:49 -0400

    [MFA]: Update Navbar Template

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,12 +1,28 @@
+import { Link } from "@reach/router";
+
 export default function Root(props) {
-    return <nav className="bg-blue-600 text-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-            <a href="#" className="text-2xl font-bold">React Navbar</a>
-            <ul className="flex space-x-4">
-                <li><a href="#" className="hover:text-gray-300">Home</a></li>
-                <li><a href="#" className="hover:text-gray-300">Admin</a></li>
-                <li><a href="#" className="hover:text-gray-300">Login</a></li>
-            </ul>
-        </div>
-    </nav>;
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <span className="text-2xl font-bold">React Navbar</span>
+        <ul className="flex space-x-4">
+          <li>
+            <Link className="hover:text-gray-300" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="hover:text-gray-300" to="/admin">
+              Admin
+            </Link>
+          </li>
+          <li>
+            <Link className="hover:text-gray-300" to="/welcome">
+              Welcome
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
